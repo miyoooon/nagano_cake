32ba81142546086f9bf096a0e4926eaea7ca5f8f
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-07-22 08:55:27 +0000

    [Add] nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
   namespace :public do
-    root to: 'homes#top'
+
   end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
