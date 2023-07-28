2db549edba23b2b49ea13174a3fd9857f1e966e3
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-07-28 16:26:32 +0000

    [Add] admin/index,admin/show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,9 +15,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def index
+    @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -7,6 +7,7 @@ Kaminari.configure do |config|
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
+    config.default_per_page = 5
   # config.page_method_name = :page
   # config.param_name = :page
   # config.max_pages = nil
