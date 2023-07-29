336305bfc5f0e7da0a7fd89987cad2f4178873c4
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-07-29 08:05:03 +0000

    [Add] public/items index,public/items show

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,17 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
+
+  def create
+    @cart_items = CartItem.new(cart_items_params)
+    @cart_items.save
+    redirect_to cart_items_path
+  end
+
+   private
+
+  def cart_items_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
